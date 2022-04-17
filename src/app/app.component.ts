05428a0435350a39a4025c9030802148a5b2587d
jp-ray <r.jay3283@gmail.com>
2022-04-18 02:15:29 +0530

    Fixed : App Title Mismatch issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator-app';
+  title = 'ScienticCalculatorWithDevOps';
   operand1: number = 0;
   operand2: number = 0;
   operator: String = "";
   expOprn: boolean = false;
   performed: boolean = false;
   sdisplay:String='';
-  
+
    keyPress(x: number) {
     if(this.performed){
       this.operand1 = x;
@@ -21,6 +21,11 @@ export class AppComponent {
       return;
     }
   }
+  sqroot() {
+    this.operand1 = Math.sqrt(this.operand1);
+    this.performed = true;
+  }
+
 
 }
 
