2865ed6e979bd562b166916ac2605179b1432d1d
jp-ray <r.jay3283@gmail.com>
2022-04-18 02:47:14 +0530

    Added : Factorial Operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,22 @@ export class AppComponent {
   performed: boolean = false;
   sdisplay:String='';
 
+  fact(n: number): any {
+    
+    if (n == 0 || n == 1)
+      return 1;
+    return this.fact(n - 1) * n;
+  }
+
+  factorial() {
+    if(!Number.isInteger(this.operand1))
+      {
+        this.sdisplay="Invalid Number!"
+        return;
+      }
+    this.operand1 = this.fact(this.operand1);
+    this.performed = true;
+  }
    keyPress(x: number) {
     if(this.performed){
       this.operand1 = x;
